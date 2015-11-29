b87fef8279bdb90e2bb271da0bb5aa694c317602
acdcjunior <acdcjunior@gmail.com>
2015-11-29 20:11:58 -0200

    Calculadora + teste JUnit

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -1,5 +1,15 @@
 package calculadora;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculadoraTest {
+
+    @Test
+    public void testSomar() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.somar(10, 50);
+        Assert.assertEquals(60, resultado);
+    }
 
 }
